1e5e7cb82be886110d7395e39fd99d317b894c29
Scavenger233 <liyiminginireland@gmail.com>
2025-05-10 18:54:25 +0100

    Add .gitignore and remove ignored files from repo

---
+++ src/main/java/com/dineflex/DineflexApplication.java
@@ -0,0 +1,13 @@
+package com.dineflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineflexApplication.class, args);
+	}
+
+}
